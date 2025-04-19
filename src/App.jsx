283e0b76d2fa802bcddc17c7a1139b115b7e9c4c
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-04-19 17:27:49 -0400

    centered?

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <div className="flex flex-col min-h-screen bg-gradient-to-tr from-[#02010B] to-[#070062]">
         <BackgroundEffect />
         <Header />
-        <main className="flex-1 relative flex">
+        <main className="flex-1 relative flex flex-col items-center">
           <PentagramProvider>
             <Routes>
               <Route path="/" element={<HeroSection />} />
